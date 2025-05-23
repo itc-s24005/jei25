9cde46da7a8a39e29b443b1e70c9238e3abe3265
itc-s24005 <s24005@std.it-college.ac.jp>
2025-05-23 11:29:16 +0900

    課題

---
+++ D49Q20kadai.java
@@ -0,0 +1,7 @@
+public class D49Q20kadai {
+    public static void main(String[] args) {
+        int[][] arr = new int[5][10];
+
+        System.out.printf("%d %d", arr.length, arr[0].length);
+    }
+}
