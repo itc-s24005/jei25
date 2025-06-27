aada54271f18d3ef93e8afbd03411be9cfefcf38
itc-s24005 <s24005@std.it-college.ac.jp>
2025-06-27 10:43:41 +0900

    問題集

--- D51Q17kadai.java
+++ D51Q17kadai.java
@@ -2,11 +2,11 @@ public class D51Q17kadai {
     public static void main(String[] args) {
         int x = 4;
         if (x > 4) {
-            System.out.printf("over 4");
+            System.out.println("over 4");
         } else if (x < 4) {
-            System.out.printf("under 4");
+            System.out.println("under 4");
         } else  {
-            System.out.printf("just 4");
+            System.out.println("just 4");
         }
     }
 }
