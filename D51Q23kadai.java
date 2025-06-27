5b030e60fdb6e7357a4025c9105c3568235589b5
itc-s24005 <s24005@std.it-college.ac.jp>
2025-06-27 12:28:28 +0900

    課題

---
+++ D51Q23kadai.java
@@ -0,0 +1,8 @@
+public class D51Q23kadai {
+    public static void main(String[] args) {
+        char[] dt = {'春', '夏', '秋', '冬'};
+        for(char a : dt){
+            System.out.print(a + "。");
+        }
+    }
+}
