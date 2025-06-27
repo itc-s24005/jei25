978a27eb64f0a6e3ad08a317e77893f461e14793
itc-s24005 <s24005@std.it-college.ac.jp>
2025-06-27 12:06:15 +0900

    課題

---
+++ D51Q21kadai.java
@@ -0,0 +1,9 @@
+public class D51Q21kadai {
+    public static void main(String[] args) {
+        // 8進数のa(10進数で12) と 16進数のb(10進数で19)　をかけて 10進数のc(6) を引く (12 x 19 - 6 で 答えは12)
+
+        int a = 014, b = 0x13, c = 6;
+        System.out.println(a * b - c);
+    }
+
+}
