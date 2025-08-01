8e990a3d02b97df45b73cf0d97040eba214a13ca
itc-s24005 <s24005@std.it-college.ac.jp>
2025-08-01 11:52:16 +0900

    課題

---
+++ D53Q21kadai.java
@@ -0,0 +1,15 @@
+public class D53Q21kadai {
+    public static void main(String[] args) {
+        byte b = 127; // 127=01111111
+        b += 1;
+        System.out.println(b);
+
+        byte a = 0b00111111;
+        byte c = 0100;       
+        byte d = 0x41;
+
+        System.out.println(a);
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
